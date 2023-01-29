55e979ecd2ec8f367c3da2533ef7578a4f04a615
New Author Name <karen.h1098@gmail.com>
2023-01-29 09:30:18 -0500

    ignore

--- frontend/src/components/BackpackerSignUp.js
+++ frontend/src/components/BackpackerSignUp.js
@@ -5,7 +5,7 @@ export default function BackpackerSignUp() {
     function handleSubmit(e) {
 
     }
-    
+
     return (
         <div className="login-wrapper">
             <h1>Please Log In</h1>
@@ -18,9 +18,7 @@ export default function BackpackerSignUp() {
                     <p>Password</p>
                     <input type="password" />
                 </label>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
+
             </form>
         </div>
     )
