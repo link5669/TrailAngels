61e67b3dab12935984085e51ff6652d141715068
link5669 <milesacq@icloud.com>
2023-01-29 07:55:35 -0500

    login storage

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -2,8 +2,13 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3003/api'
 
 const create = newObject => {
-  const request = axios.post(`${baseUrl}/users`, newObject)
-  return request.then(response => response.data)
+    const request = axios.post(`${baseUrl}/users`, newObject)
+    return request.then(response => {
+        if (response.status == 200) {
+            window.localStorage.setItem('loggedUser', response.data.userID)
+        }
+        console.log(response)
+    })
 }
 
 export default create
