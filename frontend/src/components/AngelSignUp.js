cf2083c90b2d1f911be882a2ebccfbfdeb50bc20
Miles Acquaviva <milesacq@icloud.com>
2023-01-29 10:04:06 -0500

    Update AngelSignUp.js

--- frontend/src/components/AngelSignUp.js
+++ frontend/src/components/AngelSignUp.js
@@ -1,6 +1,6 @@
 
 import React, {useState} from 'react'
-import create from '../services/users'
+import { create } from '../services/users'
 import { useNavigate } from 'react-router-dom'
 
 export default function AngelSignUp() {
