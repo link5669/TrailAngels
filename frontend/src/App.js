fa39d275fc43c62e39c295b9358edd44f5b4fa20
New Author Name <karen.h1098@gmail.com>
2023-01-28 19:35:29 -0500

    browser router implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import TrailMap from './TrailMap'
 import appalachian from './data/appalachian.geojson'
 
+import Dashboard from './components/Dashboard.js'
+import NoPage from './components/NoPage.js'
+import Home from './components/Home.js'
+import Layout from './components/Layout.js'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   console.log("rendering App")
   return (
     <div className="App">
-      <div className="web-container">
+      <h1>Trail Magic</h1>
+
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      {/* <div className="web-container">
         <TrailMap trailGeoJSON={appalachian}></TrailMap>
-      </div>
+      </div> */}
+
     </div>
   );
 }
