d9812f52e31c9d200bfbe411f796439c173ef0b0
Miles Acquaviva <milesacq@icloud.com>
2023-01-29 10:49:48 -0500

    Update AngelSpecs.js

--- frontend/src/components/AngelSpecs.js
+++ frontend/src/components/AngelSpecs.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import '../styles/AngelSpecs.css'
 import appalachian from '../data/appalachian.geojson'
 import TrailMap from './TrailMap'
+import {create} from '../services/locations'
 
 // Hard coded but should be passed in as parameter
 const trailOptions = ["Appalachian Trail"]
@@ -18,25 +19,26 @@ export default function AngelSpecs() {
     const [selectedTrail, setSelectedTrail] = useState('Trail')
  
     function handleSubmit(e) {
+        e.preventDefault();
+        create({userID: window.localStorage.getItem('loggedUser'), type: "Feature", geometry: {type: "Point", coordinates: []}, properties: {title: "Angel Station", description: "Angel Station", type: "firstAid"}})
         routeChange()
     }
 
     return (
-        <div className="specs-wrapper">
-            <h1>What Trail Magic can you provide?</h1>
-            <form onSubmit={handleSubmit}>
-            <Dropdown options={trailOptions} value={selectedTrail} onChange={(e) => {
-                setSelectedTrail(e.value);
-      }}/>
-      <div map-wrapper>
-        <TrailMap trailGeoJSON={appalachian} addMarkerEnabled={true}></TrailMap>
-      </div>
-      <div className="magic-specs-wrapper">
-        <h3>Double click on your station!</h3>
-        <h3>or Enter your coordinates:</h3>
-        <input type="number" className="lat-input" placeholder='Latitude'>
-        </input>
-        <input type="number" classname="lon-input" placeholder="Longitude"></input>
+        <Dropdown options={trailOptions} value={selectedTrail} onChange={(e) => {
+                    setSelectedTrail(e.value);
+                }} />
+                <div map-wrapper>
+                    <TrailMap trailGeoJSON={appalachian} addMarkerEnabled={true}></TrailMap>
+                </div>
+                <div className="magic-specs-wrapper">
+                    <h3>Double click on your station!</h3>
+                    <h3>or Enter your coordinates:</h3>
+                    <input type="number" className="lat-input" placeholder='Latitude'>
+                    </input>
+                    <input type="number" classname="lon-input" placeholder="Longitude"></input>
+                </div>
+                <button type="submit" className="submit-angel-specs">Submit</button>
       </div>
             <button type="submit" className="submit-angel-specs">Submit</button>
 
